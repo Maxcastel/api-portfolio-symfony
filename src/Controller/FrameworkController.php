9909f9c859ce317438b88ffa9ef1df3feb274586
Maxence Castel <maxence.nord@gmail.com>
2024-04-07 14:27:32 +0200

    add getFrameworks

--- src/Controller/FrameworkController.php
+++ src/Controller/FrameworkController.php
@@ -5,15 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use App\Repository\FrameworkRepository;
 
 class FrameworkController extends AbstractController
 {
-    #[Route('/framework', name: 'app_framework')]
-    public function index(): JsonResponse
+    #[Route('/api/frameworks', methods: ['GET'], name: 'framework_get')]
+    public function getFrameworks(FrameworkRepository $frameworkRepository, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/FrameworkController.php',
-        ]);
+        return $this->json(
+            [
+                "status" => 200,
+                "success" => true,
+                "data" => $frameworkRepository->findAll(),
+                "message" => "Operation completed with success"
+            ], 
+            Response::HTTP_OK, [], ['groups' => 'getFrameworks']
+        );
     }
 }

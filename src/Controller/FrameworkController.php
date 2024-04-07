620a633494aeee418e856a714667f52940efb0ec
Maxence Castel <maxence.nord@gmail.com>
2024-04-07 14:47:11 +0200

    add getFramework

--- src/Controller/FrameworkController.php
+++ src/Controller/FrameworkController.php
@@ -13,13 +13,27 @@ use App\Repository\FrameworkRepository;
 class FrameworkController extends AbstractController
 {
     #[Route('/api/frameworks', methods: ['GET'], name: 'framework_get')]
-    public function getFrameworks(FrameworkRepository $frameworkRepository, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
+    public function getFrameworks(FrameworkRepository $frameworkRepository): JsonResponse
     {
         return $this->json(
             [
                 "status" => 200,
                 "success" => true,
                 "data" => $frameworkRepository->findAll(),
+                "message" => "Operation completed with success"
+            ], 
+            Response::HTTP_OK, [], ['groups' => 'getFrameworks']
+        );
+    }
+
+    #[Route('/api/frameworks/{id}', methods: ['GET'], name: 'framework_get_one')]
+    public function getFramework(int $id, FrameworkRepository $frameworkRepository): JsonResponse
+    {
+        return $this->json(
+            [
+                "status" => 200,
+                "success" => true,
+                "data" => $frameworkRepository->find($id),
                 "message" => "Operation completed with success"
             ], 
             Response::HTTP_OK, [], ['groups' => 'getFrameworks']

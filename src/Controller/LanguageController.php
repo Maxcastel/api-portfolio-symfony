05d1478389267af6e80b69479b5ad56e976d01ba
Maxence Castel <maxence.nord@gmail.com>
2024-04-07 19:39:05 +0200

    add getLanguages

--- src/Controller/LanguageController.php
+++ src/Controller/LanguageController.php
@@ -5,15 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Repository\LanguageRepository;
+use App\Entity\Language;
 
 class LanguageController extends AbstractController
 {
-    #[Route('/language', name: 'app_language')]
-    public function index(): JsonResponse
+    #[Route('/api/languages', methods: ['GET'], name: 'language_get')]
+    public function getLanguages(LanguageRepository $languageRepository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/LanguageController.php',
-        ]);
+        return $this->json(
+            [
+                "status" => 200,
+                "success" => true,
+                "data" => $languageRepository->findAll(),
+                "message" => "Operation completed with success"
+            ], 
+            Response::HTTP_OK, [], ['groups' => 'getLanguages']
+        );
     }
 }

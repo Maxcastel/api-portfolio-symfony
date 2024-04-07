387ac57f9eff18f125a7db1e90f72ed760b1d169
Maxence Castel <maxence.nord@gmail.com>
2024-04-07 21:47:26 +0200

    add getCategories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,15 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Repository\CategoryRepository;
 
 class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'app_category')]
-    public function index(): JsonResponse
+    #[Route('/api/category', methods: ['GET'], name: 'category_get')]
+    public function getCategories(CategoryRepository $categoryRepository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CategoryController.php',
-        ]);
+        return $this->json(
+            [
+                "status" => 200,
+                "success" => true,
+                "data" => $categoryRepository->findAll(),
+                "message" => "Operation completed with success"
+            ], 
+            Response::HTTP_OK, [], ['groups' => 'getCategory']
+        );
     }
 }

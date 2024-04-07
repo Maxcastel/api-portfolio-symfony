7ba992049b3e98ec4f8aec8abcb19b4abd874b9e
Maxence Castel <maxence.nord@gmail.com>
2024-04-07 21:07:00 +0200

    add getType

--- src/Controller/TypeController.php
+++ src/Controller/TypeController.php
@@ -10,7 +10,7 @@ use App\Repository\TypeRepository;
 
 class TypeController extends AbstractController
 {
-    #[Route('/api/types', name: 'type_get')]
+    #[Route('/api/types', methods: ['GET'], name: 'type_get')]
     public function getTypes(TypeRepository $typeRepository): JsonResponse
     {
         return $this->json(
@@ -18,6 +18,32 @@ class TypeController extends AbstractController
                 "status" => 200,
                 "success" => true,
                 "data" => $typeRepository->findAll(),
+                "message" => "Operation completed with success"
+            ], 
+            Response::HTTP_OK, [], ['groups' => 'getTypes']
+        );
+    }
+
+    #[Route('/api/types/{id}', methods: ['GET'], name: 'type_get_one')]
+    public function getType(int $id, TypeRepository $typeRepository): JsonResponse
+    {
+        $type = $typeRepository->find($id);
+        if (!$type) {
+            return $this->json(
+                [
+                    "status" => 404,
+                    "success" => false,
+                    "message" => "Type with id $id does not exist"
+                ], 
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        return $this->json(
+            [
+                "status" => 200,
+                "success" => true,
+                "data" => $type,
                 "message" => "Operation completed with success"
             ], 
             Response::HTTP_OK, [], ['groups' => 'getTypes']

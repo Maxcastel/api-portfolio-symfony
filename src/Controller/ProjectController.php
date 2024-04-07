897fcf4c84478475b579369331a31d4cbc3e5d80
Maxence Castel <maxence.nord@gmail.com>
2024-04-07 23:29:47 +0200

    add getProject

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -23,4 +23,31 @@ class ProjectController extends AbstractController
             Response::HTTP_OK, [], ['groups' => 'getProjects']
         );
     }
+
+    #[Route('/api/projects/{id}', methods: ['GET'], name: 'project_get_one')]
+    public function getProject(int $id, ProjectRepository $projectRepository): JsonResponse
+    {
+        $project = $projectRepository->find($id);
+
+        if (!$project) {
+            return $this->json(
+                [
+                    "status" => 404,
+                    "success" => false,
+                    "message" => "Project with id $id does not exit"
+                ], 
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        return $this->json(
+            [
+                "status" => 200,
+                "success" => true,
+                "data" => $project,
+                "message" => "Operation completed with success"
+            ], 
+            Response::HTTP_OK, [], ['groups' => 'getProjects']
+        );
+    }
 }

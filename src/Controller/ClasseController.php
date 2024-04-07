1f54f883b340dbf984d873b703bb090aeb577957
Maxence Castel <maxence.nord@gmail.com>
2024-04-07 22:25:32 +0200

    add getClasses

--- src/Controller/ClasseController.php
+++ src/Controller/ClasseController.php
@@ -5,15 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Repository\ClasseRepository;
 
 class ClasseController extends AbstractController
 {
-    #[Route('/classe', name: 'app_classe')]
-    public function index(): JsonResponse
+    #[Route('/api/classes', methods: ['GET'], name: 'classe_get')]
+    public function getClasses(ClasseRepository $classeRepository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ClasseController.php',
-        ]);
+        return $this->json(
+            [
+                "status" => 200,
+                "success" => true,
+                "data" => $classeRepository->findAll(),
+                "message" => "Operation completed with success"
+            ], 
+            Response::HTTP_OK, [], ['groups' => 'getClasses']
+        );
     }
 }

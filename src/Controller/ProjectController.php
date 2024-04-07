8714888915e2108f6fb565ab022124111c460528
Maxence Castel <maxence.nord@gmail.com>
2024-04-07 23:15:26 +0200

    add getProjects

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -5,15 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Repository\ProjectRepository;
 
 class ProjectController extends AbstractController
 {
-    #[Route('/project', name: 'app_project')]
-    public function index(): JsonResponse
+    #[Route('/api/projects', methods: ['GET'], name: 'project_get')]
+    public function getProjects(ProjectRepository $projectRepository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProjectController.php',
-        ]);
+        return $this->json(
+            [
+                "status" => 200,
+                "success" => true,
+                "data" => $projectRepository->findAll(),
+                "message" => "Operation completed with success"
+            ], 
+            Response::HTTP_OK, [], ['groups' => 'getProjects']
+        );
     }
 }

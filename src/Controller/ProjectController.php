c895352b4f5dcd7a3cac738141a6ad921361d8a2
Maxence Castel <maxence.nord@gmail.com>
2024-05-03 21:52:36 +0200

    add getProjectByUrl

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -78,17 +78,81 @@ class ProjectController extends AbstractController
         );
     }
 
-    #[Route('/api/projects/{id}', methods: ['GET'], name: 'project_get_one')]
-    public function getProject(int $id, ProjectRepository $projectRepository): JsonResponse
+    // #[Route('/api/projects/{id}', methods: ['GET'], name: 'project_get_one')]
+    // public function getProject(int $id, ProjectRepository $projectRepository): JsonResponse
+    // {
+    //     $project = $projectRepository->find($id);
+
+    //     if (!$project) {
+    //         return $this->json(
+    //             [
+    //                 "status" => 404,
+    //                 "success" => false,
+    //                 "message" => "Project with id $id does not exit"
+    //             ], 
+    //             Response::HTTP_NOT_FOUND
+    //         );
+    //     }
+
+    //     $response = [
+    //         'id' => $project->getId(),
+    //         'title' => $project->getTitle(),
+    //         'url' => $project->getUrl(),
+    //         'description' => $project->getDescription(),
+    //         'content' => $project->getContent(),
+    //         'link' => $project->getLink(),
+    //         'github' => $project->getGithub(),
+    //         'date' => $project->getDate(),
+    //         'thumbnail' => $project->getThumbnail(),
+    //         'frameworks' => $project->getFrameworks()->map(
+    //             fn (Framework $framework) => [
+    //                 'id' => $framework->getId(),
+    //                 'name' => $framework->getName(),
+    //             ]
+    //         )->toArray(),
+    //         'languages' => $project->getLanguages()->map(
+    //             fn (Language $language) => [
+    //                 'id' => $language->getId(),
+    //                 'name' => $language->getName(),
+    //                 'shortName' => $language->getShortName(),
+    //             ]
+    //         )->toArray(),
+    //         'type' => [
+    //             'id' => $project->getType()->getId(),
+    //             'name' => $project->getType()->getName(),
+    //         ],
+    //         'category' => [
+    //             'id' => $project->getCategory()->getId(),
+    //             'name' => $project->getCategory()->getName(),
+    //         ],
+    //         'classe' => $project->getClasse() !== null ? [
+    //             'id' => $project->getClasse()->getId(),
+    //             'name' => $project->getClasse()->getName(),
+    //         ] : null
+    //     ];
+
+    //     return $this->json(
+    //         [
+    //             "status" => 200,
+    //             "success" => true,
+    //             "data" => $response,
+    //             "message" => "Operation completed with success"
+    //         ], 
+    //         Response::HTTP_OK
+    //     );
+    // }
+
+    #[Route('/api/projects/{url}', methods: ['GET'], name: 'project_get_one_url')]
+    public function getProjectByUrl(String $url, ProjectRepository $projectRepository): JsonResponse
     {
-        $project = $projectRepository->find($id);
+        $project = $projectRepository->findOneBy(["url" => $url]);
 
         if (!$project) {
             return $this->json(
                 [
                     "status" => 404,
                     "success" => false,
-                    "message" => "Project with id $id does not exit"
+                    "message" => "Project with url $url does not exit"
                 ], 
                 Response::HTTP_NOT_FOUND
             );

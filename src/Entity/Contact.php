04ef8c6f888b0b5cec99713388d55e9aa854223d
Maxence Castel <maxence.nord@gmail.com>
2024-05-05 13:59:02 +0200

    contact entiy: add sendDate property

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -26,6 +26,9 @@ class Contact
     #[ORM\Column(type: Types::TEXT)]
     private ?string $message = null;
 
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $sendDate = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -75,6 +78,18 @@ class Contact
     public function setMessage(string $message): static
     {
         $this->message = $message;
+
+        return $this;
+    }
+
+    public function getSendDate(): ?\DateTimeInterface
+    {
+        return $this->sendDate;
+    }
+
+    public function setSendDate(\DateTimeInterface $sendDate): static
+    {
+        $this->sendDate = $sendDate;
 
         return $this;
     }

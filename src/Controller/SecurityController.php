b15fc6add18698ccd21198588b57e05a061043aa
Maxcastel <maxence.nord@gmail.com>
2024-06-14 00:04:11 +0200

    SecurityController: add isAuth

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,15 +4,35 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\HttpFoundation\Request;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/security', name: 'app_security')]
-    public function index(): Response
+    private $tokenStorageInterface;
+    private $jwtManager;
+
+    public function __construct(TokenStorageInterface $tokenStorageInterface, JWTTokenManagerInterface $jwtManager)
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
+        $this->tokenStorageInterface  = $tokenStorageInterface;
+        $this->jwtManager = $jwtManager;
     }
+
+    #[Route('/api/isAuth', methods: ['GET'], name: 'api_isAuth')]
+    public function isAuth(): JsonResponse
+    {
+        return $this->json(
+            [   
+                "code" => 200,
+                "isAuth " => true
+            ], 
+            Response::HTTP_OK
+        );
+    }
+
 }

9a0e239d60fe099c81815644f0dc9f4a893ae87b
Maxcastel <maxence.nord@gmail.com>
2024-06-16 12:42:05 +0200

    ContactController: add try catch in sendEmail

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -13,40 +13,53 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use App\Entity\Contact;
 use App\Repository\ContactRepository;
+use Exception;
 
 class ContactController extends AbstractController
 {
     #[Route('/api/email', methods: ['POST'], name: 'send_email')]
     public function sendEmail(MailerInterface $mailer, Request $request, EntityManagerInterface $em, SerializerInterface $serializer): JsonResponse
     {
-        $data = json_decode($request->getContent(), true);
+        try{
+            $data = json_decode($request->getContent(), true);
 
-        $message = $serializer->deserialize($request->getContent(), Contact::class, 'json');
+            $message = $serializer->deserialize($request->getContent(), Contact::class, 'json');
 
-        $email = (new Email())
-            ->from($data["email"])
-            ->to('maxence.castel59@gmail.com')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
-            ->subject($data["subject"])
-            // ->text('Sending emails is fun again!')
-            ->html($data["message"]);
+            $email = (new Email())
+                ->from($data["email"])
+                ->to('maxence.castel59@gmail.com')
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject($data["subject"])
+                // ->text('Sending emails is fun again!')
+                ->html($data["message"]);
 
-        $mailer->send($email);
+            $mailer->send($email);
 
-        $em->persist($message);
-        $em->flush();
+            $em->persist($message);
+            $em->flush();
 
-        return $this->json(
-            [
-                "status" => 200,
-                "success" => true,
-                "message" => "Email send with success"
-            ], 
-            Response::HTTP_OK
-        );
+            return $this->json(
+                [
+                    "status" => 200,
+                    "success" => true,
+                    "message" => "Email send with success"
+                ], 
+                Response::HTTP_OK
+            );
+        }
+        catch(Exception $e){
+            return $this->json(
+                [
+                    "status" => 500,
+                    "success" => false,
+                    "message" => $e->getMessage()
+                ], 
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
     }
 
     #[Route('/api/emails', methods: ['GET'], name: 'emails_get')]
